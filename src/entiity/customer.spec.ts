98eaa09a0a2896d0457c21fa554e2c7c5d3c3023
Luiz Carlos Metzger <lcmetzger@gmail.com>
2022-04-17 17:19:22 -0300

    test: customers tests

---
+++ src/entiity/customer.spec.ts
@@ -0,0 +1,79 @@
+import { Address } from "./address";
+import { Customer } from "./customer";
+
+describe("Customer Test", () => {
+  it("should throw error id is empty", () => {
+    const address: Address = new Address(
+      "any_streen",
+      "any_city",
+      "any_state",
+      "any_zipcode"
+    );
+    expect(() => {
+      const customer: Customer = new Customer("", "any_name", address, true);
+    }).toThrowError("Id is required");
+  });
+
+  it("should throw error name is empty", () => {
+    const address: Address = new Address(
+      "any_streen",
+      "any_city",
+      "any_state",
+      "any_zipcode"
+    );
+    expect(() => {
+      let customer: Customer = new Customer("any_id", "", address, true);
+    }).toThrowError("Name is required");
+  });
+
+  it("should change the name", () => {
+    const address: Address = new Address(
+      "any_streen",
+      "any_city",
+      "any_state",
+      "any_zipcode"
+    );
+    const customer: Customer = new Customer(
+      "any_id",
+      "any_name",
+      address,
+      true
+    );
+    customer.changeName("Maria");
+    expect(customer.name).toBe("Maria");
+  });
+
+  it("should change to active", () => {
+    const address: Address = new Address(
+      "any_streen",
+      "any_city",
+      "any_state",
+      "any_zipcode"
+    );
+    const customer: Customer = new Customer(
+      "any_id",
+      "any_name",
+      address,
+      false
+    );
+    customer.activate();
+    expect(customer.isActive).toBeTruthy();
+  });
+
+  it("should change to deactive", () => {
+    const address: Address = new Address(
+      "any_streen",
+      "any_city",
+      "any_state",
+      "any_zipcode"
+    );
+    const customer: Customer = new Customer(
+      "any_id",
+      "any_name",
+      address,
+      true
+    );
+    customer.deactivate();
+    expect(customer.isActive).toBeFalsy();
+  });
+});
--- src/entiity/customer.ts
+++ src/entiity/customer.ts
@@ -6,17 +6,39 @@ export class Customer {
     private _name: string,
     private _address: Address,
     private _active: boolean
-  ) {}
+  ) {
+    this.validate();
+  }
+
+  validate() {
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get isActive(): boolean {
+    return this._active;
+  }
 
   changeName(name: string) {
     this._name = name;
+    this.validate();
   }
 
-  activated() {
+  activate() {
     this._active = true;
+    this.validate();
   }
 
-  deactivated() {
+  deactivate() {
     this._active = false;
+    this.validate();
   }
 }
